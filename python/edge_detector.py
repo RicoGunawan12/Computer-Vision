b32a0983692fb5deeb400e7bdc9d84633ad69f7c
Rico <ricogunawan12323@gmail.com>
2023-05-03 16:45:41 +0700

    Final

--- python/edge_detector.py
+++ python/edge_detector.py
@@ -1,42 +1,27 @@
 import cv2
-import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread("./assets/edge.webp")
+image = cv2.imread('./assets/edge.webp')
 igray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-igray = np.float32(igray)
+def showResult(nrow = None, ncol = None, res_stack = None):
+    plt.figure(figsize=(12, 12))
+    for i, (lbl, img) in enumerate(res_stack):
+        plt.subplot(nrow, ncol, i + 1)
+        plt.imshow(img, cmap='gray')
+        plt.title(lbl)
+        plt.axis('off')
 
-def showResult(source, cmap = None):
-    plt.imshow(source, cmap=cmap)
     plt.show()
 
-harris = cv2.cornerHarris(igray, 2, 5, 0.04)
+sobel_x = cv2.Sobel(igray, cv2.CV_32F, 1, 0, ksize=3)
+sobel_y = cv2.Sobel(igray, cv2.CV_32F, 0, 1, ksize=3)
 
-without_subpix = image.copy()
-without_subpix[harris > 0.01 * harris.max()] = [0, 255, 0]
+sobel_labels = ['sobel x', 'sobel y']
+sobel_images = [sobel_x, sobel_y]
 
-showResult(harris, 'gray')
-showResult(without_subpix, 'gray')
+merged_sobel = np.sqrt(np.square(sobel_x) + np.square(sobel_y))
+merged_sobel *= 255 / merged_sobel.max()
 
-
-_, thresh = cv2.threshold(harris, 0.01 * harris.max(), 255, 0)
-thresh = np.uint8(thresh)
-
-_, _, _, centroids = cv2.connectedComponentsWithStats(thresh)
-centroids = np.float32(centroids)
-
-criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 100, 0.0001)
-
-enhanced_criteria = cv2.cornerSubPix(igray, centroids, (2, 2), (2, -1), criteria)
-enhanced_criteria = np.uint16(enhanced_criteria)
-
-
-with_subpix = image.copy()
-
-for i in enhanced_criteria:
-    x, y = i[:2]
-    with_subpix[y, x] = [255, 0, 0]
-
-showResult(with_subpix, 'gray')
+showResult(1, 1, zip(['merged sobel'], [merged_sobel]))
